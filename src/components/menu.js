03a73cb1e22c4946cb94f96ff9f65f3591b62ff7
Stephen Fleming <fleming.steve72@gmail.com>
2018-11-19 12:00:15 +1100

    collapsable menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,32 +4,77 @@ import { NavLink } from "react-router-dom";
 import "./menu.scss";
 
 class Menu extends Component {
-  constructor(props) {
-	super(props);
-	
-	this.setState({menuOpen: false});
+  constructor(props, context) {
+    super(props);
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.getMenuItems = this.getMenuItems.bind(this);
+    this.getMenuBurger = this.getMenuBurger.bind(this);
+    this.isSmallScreen = this.isSmallScreen.bind(this);
+
+    this.state = {
+	  menuOpen: false,
+	  displayListItems: true
+    };
+  }
+
+  isSmallScreen() {
+    return window.innerWidth <= 768;
+  }
+
+  toggleMenu() {
+    this.state.menuOpen = !this.state.menuOpen;
+    this.forceUpdate();
+  }
+
+  getMenuBurger() {
+    let burgerClassName = "list-burger";
+
+    if (this.state.menuOpen) burgerClassName += " list-burger-abs";
+
+    return (
+      <div className="list-burger-container">
+        <ul
+          className={burgerClassName}
+          id="menu-burger"
+          onClick={this.toggleMenu}
+        >
+          <li>|</li>
+          <li>|</li>
+          <li>|</li>
+        </ul>
+      </div>
+    );
+  }
+
+  getMenuItems() {
+    if (!this.isSmallScreen() || this.state.menuOpen || this.state.displayListItems) {
+      return (
+        <div class="list-links-container">
+          <ul className="list-links right flex flex-item-container">
+            <li className="flex-item-center">
+              <NavLink to="/">HOME</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">PROJECTS</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact">CONTACT</NavLink>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+    return <div />;
   }
 
   render() {
+
     return (
       <div className="flex flex-item-wide flex-rtl">
-        <ul className="list-burger" id="menu-burger">
-          <li>|</li>
-          <li>|</li>
-          <li>|</li>
-        </ul>
-
-        <ul className="list-links right flex flex-item-container">
-          <li className="flex-item-center">
-            <NavLink to="/">HOME</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">PROJECTS</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact">CONTACT</NavLink>
-          </li>
-        </ul>
+        {this.getMenuItems()}
+        {this.getMenuBurger()}
       </div>
     );
   }
