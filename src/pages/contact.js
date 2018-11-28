5f52fc52c08b897e68084216a4e78085d71635b5
Stephen Fleming <fleming.steve72@gmail.com>
2018-11-28 14:31:13 +1100

    text-alignment on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import {GitHubLink, LinkedInLink} from "../components/social-icons";
+import React from "react";
+import { GitHubLink, LinkedInLink } from "../components/social-icons";
 
-//import "./contact.scss";
+import "./contact.scss";
 
 const Contact = () => (
-	<div className="columns">
-		<div className="column contact-fields">
-		<h2>Stephen Fleming</h2>
-		<p>Melbourne, Australia</p>
-		<br/>
-		<p>I can be reached via phone at <a href="tel:0422259905">04 222 599 05</a>,</p>
-		<p>or email at <a href="mailto:fleming.steve72@gmail.com">fleming.steve72@gmail.com</a></p>
-		</div>
-		<div className="column">
-			{<GitHubLink/>}
-			{<LinkedInLink/>}
-		</div>
-	</div>
+  <div className="columns three-quart margin-center">
+    <div className="column contact-fields">
+      <br />
+      <h2>Stephen Fleming</h2>
+      <p>Melbourne, Australia</p>
+      <br />
+      <p>
+        I can be reached via phone at <a href="tel:0422259905">04 222 599 05</a>
+        ,
+      </p>
+      <p>
+        or email at
+        <a href="mailto:fleming.steve72@gmail.com">fleming.steve72@gmail.com</a>
+      </p>
+    </div>
+    <div className="column">
+      {<GitHubLink />}
+      {<LinkedInLink />}
+    </div>
+  </div>
 );
 
 export default Contact;
