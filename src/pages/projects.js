26d9d1aebfd27f087a3dbeba0e943ee708d2f2fc
Stephen Fleming <fleming.steve72@gmail.com>
2018-11-30 11:51:02 +1100

    made projects page stateless

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class Projects extends Component {
-	render() {
-		return(
-			<h1>Projects</h1>
-		);
-	}
-}
+const Projects = () => (
+  <div>
+    <h2>Projects</h2>
+  </div>
+);
 
 export default Projects;
